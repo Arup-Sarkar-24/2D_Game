dc03e409c9696f15fe68afc293fe94854e0a7459
arup-sarkar-24 <1arupsarkar112@gmail.com>
2025-05-07 08:42:11 +0530

    Add keyboard inputs

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,12 +1,14 @@
 package main;
 
+import input.KeyboardInputs;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class GamePanel extends JPanel {
 
     public GamePanel(){
-
+        addKeyListener(new KeyboardInputs());
     }
 
     public void paintComponent(Graphics g){
