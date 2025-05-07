dbc855345169e62751ff06030984f522c84e2d90
arup-sarkar-24 <1arupsarkar112@gmail.com>
2025-05-07 08:51:46 +0530

    Add mouse inputs

--- src/input/KeyboardInputs.java
+++ src/input/KeyboardInputs.java
@@ -11,7 +11,12 @@ public class KeyboardInputs implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-
+        switch (e.getKeyCode()){
+            case KeyEvent.VK_W-> System.out.println("It's W");
+            case KeyEvent.VK_A-> System.out.println("It's A");
+            case KeyEvent.VK_S-> System.out.println("It's S");
+            case KeyEvent.VK_D-> System.out.println("It's D");
+        }
     }
 
     @Override
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,14 +1,19 @@
 package main;
 
 import input.KeyboardInputs;
+import input.MouseInputs;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class GamePanel extends JPanel {
 
+    private MouseInputs mouseInputs;
+
     public GamePanel(){
         addKeyListener(new KeyboardInputs());
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
     }
 
     public void paintComponent(Graphics g){
